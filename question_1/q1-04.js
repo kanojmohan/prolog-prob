3c05e6c1e387ad522f018df84563f75ffe36ef9c
kanojmohan <kanojmohan@gmail.com>
2021-02-25 11:51:01 +0530

    number of  elements

--- question_1/q1-04.js
+++ question_1/q1-04.js
@@ -1,3 +1,19 @@
-var juice = ["banana", "apple", "oranges", "pineapple", "jackfruit", "chikku"];
-let count = juice.length;
-console.log(count);
+// Find the number of elements of a list.
+
+const juice = [
+  "banana",
+  "apple",
+  "oranges",
+  "pineapple",
+  "jackfruit",
+  "chikku",
+];
+// let count = juice.length;
+// console.log(count);
+function numberOfElement(input) {
+  //   let count = 1;
+  for (count in input) {
+  }
+  return ++count;
+}
+console.log(numberOfElement(juice));
