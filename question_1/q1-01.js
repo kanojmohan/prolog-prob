8cab2c4f9c6e1343ca60b7cd4e76d44372f27aea
kanojmohan <kanojmohan@gmail.com>
2021-02-25 10:21:46 +0530

    second last element

--- question_1/q1-01.js
+++ question_1/q1-01.js
@@ -1,20 +1,15 @@
-// const colors = ["red", "yellow", "green", "blue", "orange"];
+// Find the last element of a list.
 
+// const colors = ["red", "yellow", "green", "blue", "orange"];
 // const lastItem = colors[colors.length - 1];
 // console.log(lastItem);
 
 var group = ["a", "b", "c", "d"];
-// var count = 0;
-// for (let item in group) {
-//   count++;
-// }
-// // console.log(group[count - 1]);
-// for (let item of group) {
-//   count++;
-// }
-// console.log(group[count - 1]);
-let item;
-for (item of group) {
-}
 
-console.log(item);
+function lastElement(input) {
+  let item;
+  for (item of input) {
+  }
+  return item;
+}
+console.log(lastElement(group));
--- question_1/q1-02.js
+++ question_1/q1-02.js
@@ -1,4 +1,10 @@
-const fruits =['apple','mango','grapes','berry', 'banana','watermelon'];
+var group = ["a", "b", "c", "d"];
+// Find the last but one element of a list.
 
-const item = fruits[fruits.length-2];
-console.log(item);
+function secondlElement(input) {
+  let item;
+  for (item in input) {
+  }
+  return input[item - 1];
+}
+console.log(secondlElement(group));
