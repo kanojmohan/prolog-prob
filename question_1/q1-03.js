92dbf5604b678685b2a3872cc07017112d7bbac2
kanojmohan <kanojmohan@gmail.com>
2021-02-25 10:48:48 +0530

    find kth element

--- question_1/q1-03.js
+++ question_1/q1-03.js
@@ -1,3 +1,10 @@
+// Find the K'th element of a list.
+
 const list = [1, 1, 2, 3, 5, 8];
-const kth = 4;
-console.log(list[kth]);
+// const kth = 4;
+// console.log(list[kth]);
+
+function kthElement(input, kth) {
+  return input[kth];
+}
+console.log(kthElement(list, 5));
